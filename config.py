4a329588fea4d493adb1d7cb00e6fbb50a5d5bd5
Mahigo-Repos <fossguru7@gmail.com>
2026-09-05 22:34:13 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+"""Configuration settings for Safenet Wi-Fi Connector."""
+
+DEFAULT_SSID = "Safenet"
+DEFAULT_AP_SSID = "Safenet_Hotspot"
+DEFAULT_AP_PASSWORD = "SafenetPassword123"
+AP_INTERFACE = "wlan0"  # Default wireless interface
+TIMEOUT_SECONDS = 20
